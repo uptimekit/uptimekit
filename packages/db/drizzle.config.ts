6b3154c73550ea5e73b6b33ff09c6a488dea4faa
stripsior <jakubkrzyz.kontakt@gmail.com>
2025-12-12 21:45:16 +0100

    fix(docker, db): update drizzle config for dev use, fix problem with building because wrong dockerignore

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,3 +1,17 @@
+import { config } from "dotenv";
+import fs from "fs";
+import path from "path";
+
+const paths = ["../../.env", "../../apps/dash/.env", ".env"];
+
+for (const p of paths) {
+	const fullPath = path.resolve(process.cwd(), p);
+	if (fs.existsSync(fullPath)) {
+		config({ path: fullPath });
+		break;
+	}
+}
+
 export default {
 	schema: "./src/schema",
 	out: "./src/migrations",
